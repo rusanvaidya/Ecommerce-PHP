c7dd798a5168c6c4305f2c3ae51b85c6fa0e2eb0
Rusan Vaidya <60218372+rusanvaidya@users.noreply.github.com>
2022-01-02 12:42:47 +0545

    Add files via upload

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$dbcon=pg_connect("host=ec2-44-194-232-228.compute-1.amazonaws.com port=5432 dbname=deep58e09o5gki user=brllexgiaifion password=4f93ab474a9add1e1ad3faadb1ee7269f6b98e1d9a31f4e15df25b609a16b7af");
+$dbcon=pg_connect("host=localhost port=5432 dbname=hkirana user=rusan password=rusan@98");
 ?>
--- index.php
+++ index.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="static/css/home.css">
     <link rel="stylesheet" href="static/css/menu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <body>
         <?php
-            include 'db.php'; 
+            include 'db_conection.php'; 
             include 'menu.php';
         ?>
         <div class="banner">
